0517554eae80ef97f0409d3b08593f6360bbe859
tarnas14 <tarnas14@gmail.com>
2015-06-22 21:39:14 +0200

    preparing logic for challenges navigation - working out responsibilities to be extracted

--- Modules/Challenges/UI/GitStyleUI.cs
+++ Modules/Challenges/UI/GitStyleUI.cs
@@ -6,45 +6,123 @@
     public class GitStyleUI
     {
         private readonly IChallengeRepository _challengeRepository;
+        private Tuple<int, int> _challengeCursor;
+        private Tuple<int, int> _displayOrigin;
+        private ChallengingDay[,] _displayArray;
+        private Tuple<int, int> _displaySize;
+        private int _displayedDaysCount;
+
+        private const int WeekColumnWidth = 2;
+        private const int DayOfTheWeekColumnWidth = 4;
 
         public GitStyleUI(IChallengeRepository challengeRepository)
         {
             _challengeRepository = challengeRepository;
+            _initialEncoding = Console.OutputEncoding;
         }
 
         public void Run()
         {
+            _displayArray = PrepareChallengeDisplayArea();
+
             Console.Clear();
+            _displayOrigin = Tuple.Create(Console.CursorLeft, Console.CursorTop);
 
+            Display(_displayOrigin, _displaySize, _displayArray, _displayedDaysCount);
+
+            Highlight(_displayOrigin, _challengeCursor);
+
+            NavigateThroughChallengeDisplay();
+        }
+
+        private static void NavigateThroughChallengeDisplay()
+        {
+            Console.CursorVisible = false;
+            bool exit = false;
+            while (!exit)
+            {
+                Console.ReadKey(true);
+            }
+        }
+
+        private void Highlight(Tuple<int, int> displayOrigin, Tuple<int, int> challengeCursor)
+        {
+            Display(() =>
+            {
+                var topOffset = displayOrigin.Item2 + challengeCursor.Item2;
+                var leftOffset = displayOrigin.Item1 + DayOfTheWeekColumnWidth + challengeCursor.Item1 * WeekColumnWidth;
+                Console.CursorLeft = leftOffset;
+                Console.CursorTop = topOffset;
+                DisplaySelected(_displayArray[challengeCursor.Item1, challengeCursor.Item2]);
+            });
+        }
+
+        private ChallengingDay[,] PrepareChallengeDisplayArea()
+        {
             var weeksToDisplay = CalculateWeeksNumberToDisplay();
-            var numberOfDaysToDisplay = CalculateNumberOfDays(weeksToDisplay);
-            var daysWithChallenge = _challengeRepository.GetLastDays(numberOfDaysToDisplay);
+            _displayedDaysCount = CalculateNumberOfDays(weeksToDisplay);
+            var daysWithChallenge = _challengeRepository.GetLastDays(_displayedDaysCount);
 
-            var initialOutputEncoding = Console.OutputEncoding;
-            Console.OutputEncoding = Encoding.UTF8;
+            _displaySize = Tuple.Create(weeksToDisplay, 7);
+
+            var displayArray = new ChallengingDay[weeksToDisplay, 7];
 
             for (int i = 0; i < 7; i++)
             {
-                Console.Write("{0} ", DaysOfTheWeek[i]);
                 for (int j = 0; j < weeksToDisplay; j++)
                 {
                     var challengeDayIndex = j*7 + i;
+
+                    if (challengeDayIndex == daysWithChallenge.Count - 1)
+                    {
+                        _challengeCursor = Tuple.Create(j, i);
+                    }
+
                     if (challengeDayIndex >= daysWithChallenge.Count)
                     {
                         continue;
                     }
 
-                    var challenge = daysWithChallenge[challengeDayIndex];
-
-                    if (challenge.NoChallenge)
-                    {
-                        Console.Write("{0} ", '\u25A1');
-                    }
+                    displayArray[j, i] = daysWithChallenge[challengeDayIndex];
                 }
-                Console.WriteLine();
             }
 
-            Console.OutputEncoding = initialOutputEncoding;
+            return displayArray;
+        }
+
+        private void Display(Tuple<int, int> displayOrigin, Tuple<int, int> displaySize, ChallengingDay[,] displayArray, int maxItemsToDisplay)
+        {
+            Display(() => {
+                Console.SetCursorPosition(displayOrigin.Item1, displayOrigin.Item2);
+                for (int i = 0; i < displaySize.Item2; i++)
+                {
+                    Console.Write("{0} ", DaysOfTheWeek[i]);
+                    for (int j = 0; j < displaySize.Item1; j++)
+                    {
+                        if (j*7 + i >= maxItemsToDisplay)
+                        {
+                            continue;
+                        }
+                    
+                        DisplayUnselected(displayArray[j,i]);
+                    }
+                    Console.WriteLine();
+                }
+            });
+        }
+
+        private void DisplaySelected(ChallengingDay challengeToDisplay)
+        {
+            var backgroundColour = Console.BackgroundColor;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("{0}", '\u25A1');
+            Console.BackgroundColor = backgroundColour;
+            Console.Write(' ');
+        }
+
+        private void DisplayUnselected(ChallengingDay challengeToDisplay)
+        {
+            Console.Write("{0} ", '\u25A1');
         }
 
         private string[] DaysOfTheWeek
@@ -74,10 +152,20 @@
 
         private int CalculateWeeksNumberToDisplay()
         {
-            const int dayOfTheWeekColumnWidth = 4;
-            const int weekColumnWidth = 2;
+            return (int)Math.Floor((double)(Console.WindowWidth - DayOfTheWeekColumnWidth - 1) / WeekColumnWidth);
+        }
 
-            return (int) Math.Floor((double)(Console.WindowWidth - dayOfTheWeekColumnWidth - 1) / weekColumnWidth);
+        private void Display(Action displayStuff)
+        {
+            var cursor = Tuple.Create(Console.CursorLeft, Console.CursorTop);
+            var encoding = Console.OutputEncoding;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            displayStuff();
+
+            Console.OutputEncoding = encoding;
+            Console.CursorLeft = cursor.Item1;
+            Console.CursorTop = cursor.Item2;
         }
     }
 }
